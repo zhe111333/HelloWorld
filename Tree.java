08fd59240399e12b282872ef6ffeb470e20c995f
zhe111333 <li458607111@gmail.com>
2023-04-12 16:40:37 +0800

    no message

---
+++ Tree.java
@@ -0,0 +1,13 @@
+public class Tree{
+	public static void main(String[] args) {
+		//Define your tree class in this file!
+		System.out.println("Hey there,I'm Ariel and I'm learning to code in Java!");
+		System.out.println("I'm going to plant a tree today!");
+		System.out.println("Ready to get my hands dirty!");
+		System.out.println("  *  ");
+		System.out.println(" *** ");
+		System.out.println(" *** ");
+		System.out.println("  *  ");
+
+	}
+}
