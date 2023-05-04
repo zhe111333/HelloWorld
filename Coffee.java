9bbfc748b3040612f18cce78508181bff7621a53
zhe111333 <li458607111@gmail.com>
2023-05-05 00:00:03 +0800

    循环

---
+++ Coffee.java
@@ -0,0 +1,13 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    
+    for (int i = 1; i <= 100; i++){
+      int cupsOfCoffee = i;
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+    }
+
+  }
+  
+}
